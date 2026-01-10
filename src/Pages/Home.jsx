9c955e7642ade7029099d219510ea5d8b663a0a9
SHAWON ISLAM <shawonislam443@gmail.com>
2026-01-10 20:03:57 +0600

    navbar

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import ThemeToggol from '../components/ThemeToggol'
 import StarBg from '../components/StarBg'
+import Navbar from '../components/NavBar'
 
 const Home = () => {
   return (
     <div className='min-h-screen bg-background text-foreground overflow-x-hidden'> 
     <ThemeToggol/>
     <StarBg/>
+    <Navbar/>
     </div>
   )
 }
